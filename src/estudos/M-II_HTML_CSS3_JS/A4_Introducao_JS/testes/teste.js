51b13955190540af611f9bc2513e979b29785d4c
Eduardo <eduardopr@ciandt.com>
2022-08-02 17:26:22 -0300

    [DIO-Santander-Bootcamp]- Js part 1-2

---
+++ src/estudos/M-II_HTML_CSS3_JS/A4_Introducao_JS/testes/teste.js
@@ -0,0 +1,21 @@
+// ==========================  manipulando um arquivo ===================
+
+// var y = 0;      "comenta uma linha"
+
+/*var x = 1;       "comenta até fechar o * /"
+var w = 2;*/
+
+var z = 3;
+var b = 2;
+var zB = z + b;
+
+console.log(zB);
+
+
+//  ======================================= funções ===================
+
+function soma(a, b) {
+    return a + b;
+}
+console.log("O resultado da função soma é: ");
+console.log(soma(2,8));
